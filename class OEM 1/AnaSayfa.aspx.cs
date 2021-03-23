ac10a938d39cc8e7d5ff0bf95390f831ea73ba1a
mertbalasar <79214655+mertbalasar@users.noreply.github.com>
2021-03-23 20:55:55 +0300

    create_app_part_1

---
+++ class OEM 1/AnaSayfa.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Collections;
+using System.Drawing;
+using System.IO;
+
+namespace class_OEM_1
+{
+    public partial class AnaSayfa : System.Web.UI.Page
+    {
+        // DEĞİŞKENLER
+
+        YönetimDb db;
+        List<string> categories;
+
+        // KENDİ OLUŞTURDUKLARIM 
+
+        private void Show_Message(string text)
+        {
+            Response.Write("<script>alert('" + text + "');</script>");
+        }
+
+        private void Load_Logo()
+        {
+            ImageButton imgButton = new ImageButton();
+            imgButton.ImageUrl = "logo2.png";
+            imgButton.Click += İmgButton_Click;
+            logo.Controls.Add(imgButton);
+        }
+
+        private void Create_Categories()
+        {
+            foreach (var item in db.Kategoriler)
+            {
+                categories.Add(item.Ad);
+                Button button = new Button();
+                button.Text = item.Ad;
+                button.CssClass = "kategori";
+                button.Attributes.Add("onmouseover", "change(this,event);");
+                button.Attributes.Add("onmouseout", "change(this,event);");
+                button.Click += Button_Click;
+                kategoriler.Controls.Add(button);
+            }  
+        }
+
+        // EVENT METODLARI
+
+        private void İmgButton_Click(object sender, ImageClickEventArgs e)
+        {
+            Response.Redirect("AnaSayfa.aspx");
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            int category = categories.IndexOf(button.Text) + 1;
+            Response.Redirect("ÜrünSayfa.aspx?category=" + category.ToString());
+        }
+
+        private void Sepetclk_ServerClick(object sender, EventArgs e)
+        {
+            Response.Redirect("SepetSayfa.aspx");
+        }
+
+        private void Bizeulasclk_ServerClick(object sender, EventArgs e)
+        {
+            Show_Message("İLETİŞİM: xxxx@gmail.com");
+        }
+
+        // SAYFA YUKLENME OLAYI
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Load_Logo();
+
+            db = new YönetimDb();
+            db.Database.CreateIfNotExists();
+            categories = new List<string>();
+
+            Create_Categories();
+
+            DropDownList1.Enabled = false;
+
+            bizeulasclk.ServerClick += Bizeulasclk_ServerClick;
+            sepetclk.ServerClick += Sepetclk_ServerClick;
+        }
+    }
+}
---
+++ class OEM 1/AnaSayfa.aspx.designer.cs
@@ -0,0 +1,116 @@
+﻿//------------------------------------------------------------------------------
+// <otomatik üretildi>
+//     Bu kod bir araç tarafından oluşturuldu.
+//
+//     Bu dosyada yapılacak değişiklikler yanlış davranışa neden olabilir ve
+//     kod tekrar üretildi. 
+// </otomatik üretildi>
+//------------------------------------------------------------------------------
+
+namespace class_OEM_1
+{
+
+
+    public partial class AnaSayfa
+    {
+
+        /// <summary>
+        /// form1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// logo denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel logo;
+
+        /// <summary>
+        /// kategoriler denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel kategoriler;
+
+        /// <summary>
+        /// sepetclk denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton sepetclk;
+
+        /// <summary>
+        /// bizeulasclk denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton bizeulasclk;
+
+        /// <summary>
+        /// Label1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// DropDownList1 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+
+        /// <summary>
+        /// Label2 denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Minimum denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Minimum;
+
+        /// <summary>
+        /// Maksimum denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Maksimum;
+
+        /// <summary>
+        /// divicerik denetimi.
+        /// </summary>
+        /// <remarks>
+        /// Otomatik üretilmiş alan.
+        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel divicerik;
+    }
+}
